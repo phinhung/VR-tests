90fa28b885c3199ca4010018527efbe4a4d7cff9
Phi Nhung <ngnhung44@gmail.com>
2018-04-14 19:17:17 +0200

    SnapZone ioCheck1

--- Assets/VRTK/Prefabs/Resources/Scripts/VRTK_SnapDropZone.cs
+++ Assets/VRTK/Prefabs/Resources/Scripts/VRTK_SnapDropZone.cs
@@ -287,6 +287,8 @@ namespace VRTK
 		public float Distancele;
 		private Vector3 center; 
 
+		GameObject checkObject1;
+
         protected virtual void Update()
         {
 			GameObject left = GameObject.Find ("LeftHand");
@@ -295,6 +297,9 @@ namespace VRTK
 
 			GameObject right = GameObject.Find ("RightHand");
 			posright = right.GetComponent<PositionRightHand> ().positionright;
+
+			var ioCheck1 = checkObject1.GetComponentInParent<VRTK_InteractableObject>();
+			if (ioCheck1.IsGrabbed==true) 
 
 			Distanceri = Vector3.Distance (posright,center);
 			Debug.Log (Distanceri);
@@ -494,35 +499,37 @@ namespace VRTK
 			
 				VRTK_InteractableObject ioCheck = ValidSnapObject (collider.gameObject, false);
 				//If the item is in a snappable position and this drop zone isn't snapped and the collider is a valid interactable object
-			if (Distancele > allowedDistance | Distanceri > allowedDistance  ) {
-				if (willSnap && !isSnapped && ioCheck != null) {
-					//Only snap it to the drop zone if it's not already in a drop zone
-					if (!ioCheck.IsInSnapDropZone ()) {
-						if (highlightObject != null) {
-							//Turn off the drop zone highlighter
-							highlightObject.SetActive (false);
+			if (Distancele > allowedDistance |( Distanceri > allowedDistance)) {
+				
+					if (willSnap && !isSnapped && ioCheck != null) {
+						//Only snap it to the drop zone if it's not already in a drop zone
+						if (!ioCheck.IsInSnapDropZone ()) {
+							if (highlightObject != null) {
+								//Turn off the drop zone highlighter
+								highlightObject.SetActive (false);
+							}
+
+							Vector3 newLocalScale = GetNewLocalScale (ioCheck);
+							if (transitionInPlace != null) {
+								StopCoroutine (transitionInPlace);
+							}
+
+							isSnapped = true;
+							currentSnappedObject = ioCheck.gameObject;
+							if (cloneNewOnUnsnap) {
+								CreatePermanentClone ();
+							}
+
+							transitionInPlace = StartCoroutine (UpdateTransformDimensions (ioCheck, highlightContainer, newLocalScale, snapDuration));
+
+							ioCheck.ToggleSnapDropZone (this, true);
 						}
+					
 
-						Vector3 newLocalScale = GetNewLocalScale (ioCheck);
-						if (transitionInPlace != null) {
-							StopCoroutine (transitionInPlace);
-						}
-
-						isSnapped = true;
-						currentSnappedObject = ioCheck.gameObject;
-						if (cloneNewOnUnsnap) {
-							CreatePermanentClone ();
-						}
-
-						transitionInPlace = StartCoroutine (UpdateTransformDimensions (ioCheck, highlightContainer, newLocalScale, snapDuration));
-
-						ioCheck.ToggleSnapDropZone (this, true);
-					}
+					//Force reset isSnapped if the item is grabbed but isSnapped is still true
+					isSnapped = (isSnapped && ioCheck && ioCheck.IsGrabbed () ? false : isSnapped);
+					wasSnapped = false;
 				}
-
-				//Force reset isSnapped if the item is grabbed but isSnapped is still true
-				isSnapped = (isSnapped && ioCheck && ioCheck.IsGrabbed () ? false : isSnapped);
-				wasSnapped = false;
 			}
 		}
 
